9a151b483a7a8101cabbe77104e22224ce591c78
CoursesTec <devwebakls@gmail.com>
2025-07-23 17:57:38 -0500

    fire base

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -6,7 +6,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDUMTm5ksASiDCOHE0nyjMJ_d9eaOZOsHw",
   authDomain: "coursetech-3403b.firebaseapp.com",
   projectId: "coursetech-3403b",
-  storageBucket: "coursetech-3403b.appspot.com",
+  storageBucket: "coursetech-3403b.firebasestorage.app",
   messagingSenderId: "184929937348",
   appId: "1:184929937348:web:ddd5ef53f642bf2c36a691",
   measurementId: "G-8JNDG6MDN7"
